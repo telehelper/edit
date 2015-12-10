9cf1e2e4d63b8ea264bb48ccb3e514c0deedac9e
telehelper <mohammad.reallm00@gmail.com>
2015-12-10 13:22:27 +0330

    Update and rename bot/seedbot.lua to telehelperbot.lua

--- telehelperbot.lua
+++ telehelperbot.lua
@@ -1,7 +1,6 @@
 package.path = package.path .. ';.luarocks/share/lua/5.2/?.lua'
   ..';.luarocks/share/lua/5.2/?/init.lua'
 package.cpath = package.cpath .. ';.luarocks/lib/lua/5.2/?.so'
-
 require("./bot/utils")
 
 VERSION = '1.0'
